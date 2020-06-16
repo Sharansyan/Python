c3466a4644266eeae273b8c4881663370bec8e8a
Sharanjit Singh <Sharanjitsinghsyan@gmail.com>
2020-06-16 17:04:02 +0530

    Create Week 2-3.py

---
+++ NPTEL/The Joy of Using Python/Assignment/Week 2/3/Week 2-3.py
@@ -0,0 +1,20 @@
+# Your last recorded submission was :
+a,b = [int(x) for x in input().split()]
+if (a>b):
+    print(b)
+if (b>a):
+    print(a)
+if (b==a):
+    print(a)
+
+
+
+# Sample solutions (Provided by instructor)
+x,y = input().split(" ")
+x = int(x)
+y = int(y)
+
+if(x<y):
+    print(x)
+else:
+    print(y)
