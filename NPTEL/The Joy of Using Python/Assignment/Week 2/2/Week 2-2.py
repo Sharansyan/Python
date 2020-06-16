2a589de710526af3b30ed9f7add9f10db8db5db2
Sharanjit Singh <Sharanjitsinghsyan@gmail.com>
2020-06-16 17:01:40 +0530

    Create Week 2-2.py

---
+++ NPTEL/The Joy of Using Python/Assignment/Week 2/2/Week 2-2.py
@@ -0,0 +1,11 @@
+# Your last recorded submission was :
+a,b = [int(x) for x in input().split()]
+c=a+b
+print(c)
+
+# Sample solutions (Provided by instructor)
+x,y = input().split(" ")
+x = int(x)
+y = int(y)
+
+print(x+y)
