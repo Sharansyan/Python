4d5ba8855bca9c0b59fba1222d3eada7e7ebc967
Sharanjit Singh <Sharanjitsinghsyan@gmail.com>
2020-06-17 10:36:09 +0530

    Create Week 6-1.py

---
+++ NPTEL/The Joy of Using Python/Assignment/Week 6/1/Week 6-1.py
@@ -0,0 +1,23 @@
+# Your last recorded submission was :
+N = int(input())
+a= list(map(int,input().split()))
+k= int(input())
+P=a[k-1]
+a.sort()
+for i in range (len(a)):
+    if a[i]==P:
+        print(i+1)
+        break
+
+
+
+# Sample solutions (Provided by instructor)
+n=int(input())
+a=[int(x) for x in input().split()]
+k=int(input())
+key=a[k-1]
+a.sort()
+for i in range(len(a)):
+    if key==a[i]:
+        print(i+1)
+        break
