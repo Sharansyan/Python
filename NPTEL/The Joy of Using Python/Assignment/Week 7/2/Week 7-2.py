e9100d181271404d949f0a755e768a72e62e17d7
Sharanjit Singh <Sharanjitsinghsyan@gmail.com>
2020-06-17 10:59:42 +0530

    Create Week 7-2.py

---
+++ NPTEL/The Joy of Using Python/Assignment/Week 7/2/Week 7-2.py
@@ -0,0 +1,37 @@
+# Your last recorded submission was :
+n=int(input());
+a=[list(map(int,input().split())) for i in range(n)];
+i=0;
+sym='YES';
+while(i<n-1):
+  j=i+1;
+  while(j<n):
+    if(a[i][j]!=a[j][i]):
+      sym='NO';
+      break;
+    j+=1;
+  i+=1;
+print(sym,end='');
+
+
+
+# Sample solutions (Provided by instructor)
+def isSymmetric(mat, N): 
+    for i in range(N): 
+        for j in range(N): 
+            if (mat[i][j] != mat[j][i]): 
+                return False
+    return True
+   
+
+a = int(input())
+
+
+m = []
+for i in range(1,a+1):    
+    l = list(map(int, input ().split ()))
+    m.append(l)
+if (isSymmetric(m, a)): 
+    print("YES")
+else: 
+    print("NO")
