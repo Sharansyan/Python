222fdd66ee619517958b6b41c118c145fa076785
Sharanjit Singh <Sharanjitsinghsyan@gmail.com>
2020-06-17 12:09:34 +0530

    Update Week 3-2.py

--- NPTEL/The Joy of Using Python/Assignment/Week 3/2/Week 3-2.py
+++ NPTEL/The Joy of Using Python/Assignment/Week 3/2/Week 3-2.py
@@ -1,1 +1,14 @@
+# Your last recorded submission was :(Working)
+x=[int(input().split(" "))]
+x.sort()
+print(1,len(x)-1)
 
+
+
+
+# Sample solutions (Provided by instructor)
+a = [int(x) for x in input().split()]
+
+a.sort() #this command sorts the list in ascending order
+
+print(a[-2],a[1])
