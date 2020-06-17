931dee0f345ea763f0afbb8a2a335e562f444e8d
Sharanjit Singh <Sharanjitsinghsyan@gmail.com>
2020-06-17 09:50:09 +0530

    Create Week 3-3.py

---
+++ NPTEL/The Joy of Using Python/Assignment/Week 3/3/Week 3-3.py
@@ -0,0 +1,25 @@
+# Your last recorded submission was :
+x=[]
+x=input().split()
+x=int(x)
+for i in x(len(x)):
+    if (i%5==0):
+        del x(i)
+print(x)
+
+
+
+# Sample solutions (Provided by instructor)
+a = [int(x) for x in input().split()]
+
+b = []
+
+for i in a:
+    if(i%5!=0):
+        b.append(i)
+
+for i in range(len(b)):
+    if(i==len(b)-1):
+        print(b[i],end="")
+    else:
+        print(b[i],end=" ")
