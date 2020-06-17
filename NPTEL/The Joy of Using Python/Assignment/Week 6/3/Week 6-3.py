4d268cd73cd2e4d1fe527d490b2dd0bdaefb538d
Sharanjit Singh <Sharanjitsinghsyan@gmail.com>
2020-06-17 10:40:18 +0530

    Create Week 6-3.py

---
+++ NPTEL/The Joy of Using Python/Assignment/Week 6/3/Week 6-3.py
@@ -0,0 +1,16 @@
+# Your last recorded submission was :
+A=int(input())
+printDict={x:x*x for x in range(1,A+1)}
+print(printDict,end="")
+printDict()
+
+
+
+# Sample solutions (Provided by instructor)
+def printDict():
+    n = int(input())
+    d=dict()
+    for i in range(1,n+1):
+        d[i]=i**2
+    print(d)
+printDict()
