3cb8520b5cbb8a1b124e92e0450d707961e586ca
Sharanjit Singh <Sharanjitsinghsyan@gmail.com>
2020-06-17 11:52:06 +0530

    Create Week 12-3.py

---
+++ NPTEL/The Joy of Using Python/Assignment/Week 12/3/Week 12-3.py
@@ -0,0 +1,16 @@
+# Your last recorded submission was :
+email = input()
+
+index1 = email.index('@')
+index2 = email.index('.')
+
+print(email[index1+1:index2],end='')
+
+
+
+# Sample solutions (Provided by instructor)
+import re
+emailAddress = input()
+pat2 = "(\w+)@(\w+)\.(com)"
+r2 = re.match(pat2,emailAddress)
+print(r2.group(2))
