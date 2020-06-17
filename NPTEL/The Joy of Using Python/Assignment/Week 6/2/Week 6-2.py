4d473d2395df05bd9da4efcb69db0f26bc96f9b3
Sharanjit Singh <Sharanjitsinghsyan@gmail.com>
2020-06-17 10:39:03 +0530

    Create Week 6-2.py

---
+++ NPTEL/The Joy of Using Python/Assignment/Week 6/2/Week 6-2.py
@@ -0,0 +1,14 @@
+# Your last recorded submission was :
+n=int(input())
+d={x:x*x*x for x in range(1,n+1)}
+print(d,end="")
+
+
+
+# Sample solutions (Provided by instructor)
+n=int(input())
+d=dict()
+for i in range(1,n+1):
+    d[i]=i*i*i
+
+print(d)
